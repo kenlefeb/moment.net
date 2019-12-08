a9f6599cf259cc88d9f9a2558fa3dd3100fff41f
Ken LeFebvre <kenneth@lefebvre.us>
2019-12-08 01:06:32 -0500

    Fix CalendarTimeTests
    Several of these tests failed when the input dates were of DateTimeKind.Unspecified, so I changed them to be DateTimeKind.Local.

--- tests/CalendarTime.Tests.cs
+++ tests/CalendarTime.Tests.cs
@@ -1,7 +1,7 @@
-using System;
 using moment.net.Models;
 using NUnit.Framework;
 using Shouldly;
+using System;
 
 namespace moment.net.Tests
 {
@@ -31,24 +31,24 @@ namespace moment.net.Tests
         [Test]
         public void CalendarTimeFromTwoFixedDates()
         {
-            var initialDate = new DateTime(2012,12,12);
-            var nextDate = new DateTime(2012,12,18);
+            var initialDate = new DateTime(2012, 12, 12, 0, 0, 0, DateTimeKind.Local);
+            var nextDate = new DateTime(2012, 12, 18, 0, 0, 0, DateTimeKind.Local);
             initialDate.CalendarTime(nextDate).ShouldStartWith(nextDate.ToString("dddd 'at' "));
         }
 
         [Test]
         public void CalendarTimeToTwoFixedDates()
         {
-            var earlierDate = new DateTime(2012,12,12);
-            var laterDate = new DateTime(2012,12,18);
+            var earlierDate = new DateTime(2012, 12, 12, 0, 0, 0, DateTimeKind.Local);
+            var laterDate = new DateTime(2012, 12, 18, 0, 0, 0, DateTimeKind.Local);
             laterDate.CalendarTime(earlierDate).ShouldStartWith(earlierDate.ToString("'Last' dddd 'at' "));
         }
 
         [Test]
         public void CalendarTimeForEcessiveTimeSpanWithSpecifiedFormat()
         {
-            var initialDate = new DateTime(2012,12,12);
-            var nextDate = new DateTime(2018,12,12);
+            var initialDate = new DateTime(2012, 12, 12, 0, 0, 0, DateTimeKind.Local);
+            var nextDate = new DateTime(2018, 12, 12, 0, 0, 0, DateTimeKind.Local);
             initialDate.CalendarTime(nextDate, new CalendarTimeFormats("", "", "", "", "", "dd/MM/yyyy")).ShouldBe(nextDate.ToString("dd/MM/yyyy"));
         }
     }
